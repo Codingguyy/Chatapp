8b8ce5b2094aac75f57d484f1db3b42afc273666
Codingguyy <amulyasharma0987@gmail.com>
2026-07-08 16:03:53 +0530

    Update layout.tsx

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -1,8 +1,10 @@
 import Chatdashboard from '@/components/chat'
+import Floatingnavbar from '@/components/floatingnavbar'
 export default async function Layout({children}:Readonly<{children:React.ReactNode}>){
     return(
-        <div className="max-w-9xl w-screen flex items-stretch bg-[#0a0a0a] no-scrollbar h-[calc(100vh-3.5rem)]">
-            <Chatdashboard/>
+        <div className="relative max-w-9xl w-screen flex items-stretch bg-[#0a0a0a] no-scrollbar h-[calc(100vh-3.5rem)]">
+             <Floatingnavbar/>
+            <Chatdashboard/> 
             {children}
         </div>
     )
